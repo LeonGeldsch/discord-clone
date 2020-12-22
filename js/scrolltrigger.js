2a891806118cf92326013da7b1dcc53abdff3978
LeonGeldsch <leon.geldschlaeger@gmail.com>
2020-12-22 20:16:14 +0100

    added scrolltrigger animation

--- js/scrolltrigger.js
+++ js/scrolltrigger.js
@@ -1,2 +1,29 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.from("#section1", {
+    duration:.8, y:"10vh", opacity:0, ease:"power2",
+        scrollTrigger: {
+            trigger:"#section1"
+        }
+})
+
+gsap.from("#section2", {
+    duration:1, y:"10vh", opacity:0, ease:"power2",
+        scrollTrigger: {
+            trigger:"#section2"
+        }
+})
+
+gsap.from("#section3", {
+    duration:1, y:"10vh", opacity:0, ease:"power2",
+        scrollTrigger: {
+            trigger:"#section3"
+        }
+})
+
+gsap.from("#section4", {
+    duration:1, y:"10vh", opacity:0, ease:"power2",
+        scrollTrigger: {
+            trigger:"#section4"        
+        }
+})
